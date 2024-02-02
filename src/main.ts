29da81863d9afb10efd549e5a6e3f92011ced9e4
kkeeth <zensin0082@gmail.com>
2024-02-02 15:47:29 +0900

    feat: Hello riotjs

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as riot from 'riot'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import App from './app.riot'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const mountApp = riot.component(App)
+const app = mountApp(
+  document.getElementById('root'),
+  { message: 'Hello World', items: [] }
+)
