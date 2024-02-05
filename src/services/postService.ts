7beee51a7829cbf6e723952e7788238d7fe291c3
kkeeth <zensin0082@gmail.com>
2024-02-06 00:28:28 +0900

    refacor: re-write arrow function

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -14,16 +14,14 @@ interface Post {
   icon: string;
 }
 
-// タイムラインへの投稿
-async function postToTimeline(post: Post): Promise<void> {
+const postToTimeline = async (post: Post): Promise<void> => {
   await addDoc(collection(db, "posts"), {
     ...post,
     timestamp: Timestamp.now(),
   });
-}
+};
 
-// タイムラインの投稿を取得
-async function getTimelinePosts(): Promise<Post[]> {
+const getTimelinePosts = async (): Promise<Post[]> => {
   const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
   const querySnapshot = await getDocs(q);
   return querySnapshot.docs.map((doc) => ({
@@ -31,6 +29,6 @@ async function getTimelinePosts(): Promise<Post[]> {
     ...doc.data(),
     timestamp: doc.data().timestamp.toDate(),
   })) as unknown as Post[];
-}
+};
 
 export { postToTimeline, getTimelinePosts };
