f0db54835a1ce4dc099cee850018294fd7179757
kkeeth <zensin0082@gmail.com>
2024-02-06 00:34:21 +0900

    feat: error handling

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -15,20 +15,30 @@ interface Post {
 }
 
 const postToTimeline = async (post: Post): Promise<void> => {
-  await addDoc(collection(db, "posts"), {
-    ...post,
-    timestamp: Timestamp.now(),
-  });
+  try {
+    await addDoc(collection(db, "posts"), {
+      ...post,
+      timestamp: Timestamp.now(),
+    });
+  } catch (e) {
+    console.error("Failed to create post: ", e);
+    throw new Error("Unable to post to timeline.");
+  }
 };
 
 const getTimelinePosts = async (): Promise<Post[]> => {
-  const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
-  const querySnapshot = await getDocs(q);
-  return querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-    timestamp: doc.data().timestamp.toDate(),
-  })) as unknown as Post[];
+  try {
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+      timestamp: doc.data().timestamp.toDate(),
+    })) as unknown as Post[];
+  } catch (e) {
+    console.error("Error getting timeline posts: ", e);
+    throw new Error("Unable to retrieve timeline posts.");
+  }
 };
 
 export { postToTimeline, getTimelinePosts };
